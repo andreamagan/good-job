e393cc4d2a619871c35f23ddec5f4e5c85a4ec06
Andrea <amr237308@gmail.com>
2020-01-16 21:57:32 +0100

    Database connection configured

--- project/__init__.py
+++ project/__init__.py
@@ -1,3 +1,9 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+from project.config import ConfigDB
 
 app = Flask(__name__)
+app.config.from_object(ConfigDB)
+
+db = SQLAlchemy(app)
---
+++ project/config.py
@@ -0,0 +1,3 @@
+class ConfigDB(object):
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://user:p4ssw0rd@localhost:3306/goodjob'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
